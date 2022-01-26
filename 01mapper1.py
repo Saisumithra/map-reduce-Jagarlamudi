296362f33d161cc32a4fb39809b6b98db12fd8cf
Saisumithra <77755086+Saisumithra@users.noreply.github.com>
2022-01-26 14:05:09 -0600

    initial commit

--- 01mapper1.py
+++ 01mapper1.py
@@ -2,14 +2,15 @@
 
 # This is example mapper
 
+
 f = open("purchases.txt","r")  # open file, read-only
-o = open("out01.txt", "w") # open file, write
+o = open("out1.txt", "w") # open file, write
 for line in f:  
-    rowList = line.strip().split("    ") 
-    print (rowList )
-    print (len(rowList ))
+    dataList = line.strip().split("    ") 
+    print (dataList )
+    print (len(dataList ))
     if len(dataList) == 6:
-        date, time, location, dept, amount, payType = rowList  #assign names
+        date, time, location, dept, amount, payType = dataList  #assign names
         print ("{0}\t{1}".format(location, amount))
         o.write("{0}\t{1}\n".format(location, amount))
 f.close()
